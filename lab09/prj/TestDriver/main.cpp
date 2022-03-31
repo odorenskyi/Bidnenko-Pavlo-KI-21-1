8c369b63991de39ea07d48200af54f880d0038cd
palllkaa <pashabidnenko13@gmail.com>
2022-03-31 22:17:32 +0300

    Створив Тест-драйвер

---
+++ lab09/prj/TestDriver/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+#include "ModulesBidnenko.h"
+
+using namespace std;
+
+int main()
+{
+    system("chcp 65001 & cls");
+    return 0;
+}
