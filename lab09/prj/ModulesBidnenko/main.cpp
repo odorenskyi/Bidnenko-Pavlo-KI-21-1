43f39c1efc1608ba65fe41d6ae7fffac0a41ea75
palllkaa <pashabidnenko13@gmail.com>
2022-03-31 23:35:06 +0300

    Налагоджено роботу завдання 10.1

--- lab09/prj/ModulesBidnenko/main.cpp
+++ lab09/prj/ModulesBidnenko/main.cpp
@@ -1,5 +1,7 @@
 #include "ModulesBidnenko.h"
 #include <cmath>
+#include <iostream>
+using namespace std;
 int s_calculation(int x, int z)
 {
     return pow(3 * sin(sqrt((12 * pow(x, 2)) / (log(x - 3)))), 2) + 0.5 * z;
@@ -8,20 +10,23 @@ deposit depositGRN(float money, unsigned int months)
 {
 	deposit d;
 	if (months < 6) {
-		return { -1, -1 };
+		return { 0, 0 };
 	}
 	else {
-		int amountHalfYear = months % 6;
-		int amountYear = amountHalfYear % 12;
+		int amountHalfYear = months / 6;
+		int amountYear = months / 12;
 		amountHalfYear = amountHalfYear - (amountYear * 2);
 
+		cout << "amountYear = " << amountYear << endl;
+        cout << "amountHalfYear = " << amountHalfYear << endl;
 		if (amountYear < 1) {
 			d.percents = 19.5;
 			d.dailypay = (money * 0.195) / 133;
 			return d;
 		}
 		else {
-			d.percents = (19.5 * amountHalfYear) + (27.0 * amountHalfYear);
+        if (amountHalfYear <= 0 && amountYear > 0) { amountHalfYear = 0; }
+			d.percents = (19.5 * amountHalfYear) + (27.0 * amountYear);
 			d.dailypay = (((money * 0.195) * amountHalfYear) + ((money * 0.270) * amountYear))
 					/ ((amountHalfYear + (amountYear * 2)) * 30);
 			return d;
--- lab09/prj/TestDriver/main.cpp
+++ lab09/prj/TestDriver/main.cpp
@@ -8,5 +8,9 @@ using namespace std;
 int main()
 {
     system("chcp 65001 & cls");
+    deposit d;
+    d = depositGRN(15000000.00, 30);
+    cout << "percents = " << d.percents << endl
+    << "daily = " << d.dailypay << endl << endl;
     return 0;
 }
